e7893e0695e45f23d6a6849c656036a502118bf3
Nathaniel Wiley <nwiley86@gmail.com>
2017-11-01 17:03:46 -0400

    Base functionality with some nuance

--- rngine.js
+++ rngine.js
@@ -2,33 +2,34 @@
 	var has_ = (typeof(_) === 'function' && typeof(_.random) === 'function')? true : false,
 		alog = [],// I/O Log Array
 		adbg = [];// Debug Array
+	function logIO (i,o){alog.push([i,o,DATE.getTime()]);}
+	function logDbg (i,o){adbg.push([i,o,DATE.getTime()]);}
 	function primitiveRNG (min,max){
 		// Ensure arguments are not undefined
 		min = Number(min) || 0;
 		max = Number(max) || 0;
 		if(min > max){// Make Sure Minimum Value is Lower Than Maximum Value
-			oldMin = min;
-			oldMax = max;
+			var oldMin = min, oldMax = max;
 			min = oldMax;
 			max = oldMin;
 		}
 		// The two steps above will mimic Underscore.random's single-argument functionality by return a value between 0 and the single argument
 		min = Math.ceil(min);
 		max = Math.floor(max);
-		return Math.random() * (max - min + 1) + min;
+		return Math.floor(Math.random() * (max - min + 1)) + min;
 		// Return Integer Between Two Values, Inclusive
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 	}
 	var genRN = (has_)? _.random : primitiveRNG;
-	function logIO (i,o){
-		alog.push([i,o]);
-	}
 	function arrayify (x){
 		x = x || [];
-		if(typeof(x) !== 'array') x = x.split(',');
+		if(typeof(x) === 'string'){
+			x = x.split(',');
+		} else if(typeof(x) !== 'object' || !x.length){
+			x = [];
+		}
 		return x;
 	}
-	adbg.push(has_);
 	var RNG = {
 		roll: function (cmd){
 			var face = 0;
@@ -40,33 +41,33 @@
 			logIO(cmd,face);
 			return face;
 		},
-		oSpinners:{
-
+		custom: {
+			roll: function (name){
+				var face = 0;
+				if(RNG.custom[name]){
+					var faceIdx = genRN(RNG.custom[name].length-1);
+					face = RNG.custom[name][faceIdx];
+				}
+				logIO(name,face);
+				return face;
+			},
+			add: function (name,aSet){
+				if(typeof(name) === 'string' && name.length && name !== 'roll' && name !== 'add') RNG.custom[name] = arrayify(aSet);
+			}
 		},
-		addSpinner: function (aSet,name){
-			if(typeof(name) === 'string' && name.length) RNG.oSpinners[name] = arrayify(aSet);
-		},
-		spin: function (spinnerName){
-
-		},
-		oDecks:{
-
-		},
-		addDeck: function (aSet,name){
-			aSet = arrayify(aSet);
-
-		},
-		draw: function (deckName){
+		/*draw: function (deckName){
 			var card = '';
 			logIO(deckName,card);
 			return card;
-		},
-		log: function (){
-			return alog;
-		},
-		debug: function (){
-			console.log(adbg);
+		},*/
+		logs: {
+			io: function (){return alog;},
+			debug: function (){return adbg;}
 		}
 	};
+	/*	Custom Prebuilds	*/
+		RNG.custom.add('fsym',['+','-',' ','+','-',' ']);
+		RNG.custom.add('fnum',[1,-1,0,1,-1,0]);
+		RNG.custom.add('percentile',['00','10','20','30','40','50','60','70','80','90']);
 	return RNG;
 })();
